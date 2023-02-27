ae31bd0a62f28d1cc8be792a042f150cd169536f
Franklin Sarmiento <franklinitiel@gmail.com>
2023-02-26 23:02:45 -0500

    core application has been added.

---
+++ core/settings_apps.py
@@ -0,0 +1,15 @@
+DJANGO_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    # 'django.contrib.admin',
+)
+
+THIRD_PARTY_APPS = tuple()
+
+CUSTOM_APPS = tuple()
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + CUSTOM_APPS
+
+
