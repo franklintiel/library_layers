55fd41b52591af2e8038ae74861e4b237d82e733
sarmient <franklin.sarmiento.ext@nokia.com>
2023-02-27 17:48:42 -0500

    settings.py files have been updated.

--- core/settings.py
+++ core/settings.py
@@ -54,19 +54,9 @@ ADMIN_MEDIA_PREFIX = '/media/'
 SECRET_KEY = 'f(^qv#b60psi(@bcqc=%r-d*gh)j883ghu1c5&ju^(((_ao52%'
 
 # List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
-)
 
-MIDDLEWARE_CLASSES = (
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-)
+# SETTINGS CONSTANTS EXTRACT FROM LIBRARY LAYERS 
+from library_layers.libraries.django.settings import *
 
 ROOT_URLCONF = 'myapp.urls'
 
