a40199f381eb1fc11f208a95aefe38e58f3b6e90
sarmient <franklin.sarmiento.ext@nokia.com>
2023-02-27 17:51:03 -0500

    layer 2_0_13 has been updated.

---
+++ library_layers/layers2_0_13/libraries/django/settings.py
@@ -0,0 +1,15 @@
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+#     'django.template.loaders.eggs.Loader',
+)
+
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+)
+
