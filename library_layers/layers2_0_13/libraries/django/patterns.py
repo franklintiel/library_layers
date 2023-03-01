afcb3e806085f125f89179b44ff3ad73020be89a
sarmient <franklin.sarmiento.ext@nokia.com>
2023-03-01 07:21:29 -0500

    Configuring migration and admin section for django 2.0.13

--- library_layers/layers2_0_13/libraries/django/patterns.py
+++ library_layers/layers2_0_13/libraries/django/patterns.py
@@ -1,8 +1,12 @@
-from .functions import include, url
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
+
+from .functions import include, url
+
 
 urlpatterns = [
     url('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
