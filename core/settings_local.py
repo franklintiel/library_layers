1c16b9ad4b570c9a13925255e7a10eea747c7aa9
sarmient <franklin.sarmiento.ext@nokia.com>
2023-03-01 07:27:52 -0500

    The settings local has been adjusted to create connection to database dinamically.

--- core/settings_local.py
+++ core/settings_local.py
@@ -1,10 +1,16 @@
+DJANGO_VERSION = os.environ.get('DJANGO_VERSION', None)
+
+if not DJANGO_VERSION:
+    raise AttributeError("The environment variable DJANGO_VERSION does not exists.")
+
+database_name = 'db{database_name}.sqlite3'.format(database_name=DJANGO_VERSION)
+dbname = "{base_dir}/dbs/{database_name}".format(base_dir=BASE_DIR, database_name=database_name)
 
 DATABASES={
     'default': {
         'ENGINE': "django.db.backends.sqlite3",
-        'NAME': os.path.join(BASE_DIR, 'db127.sqlite3'),
+        'NAME': dbname,
     }
 }
 
 ROOT_URLCONF = 'core.urls'
-
